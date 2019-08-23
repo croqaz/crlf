34e5bb621fcce609e19e74d0e9db1cd7c2fda86b
Cristi Constantin <cristi.constantin@sent.com>
2019-08-23 21:42:35 +0100

    Added basic configs for Tailwind and PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './_includes/*.njk',
+    './_layouts/*.njk',
+    './_pages/*.njk',
+    './log/*.md',
+    './log/notes/*.md',
+    './log/articles/*.md',
+  ],
+  whitelist: ['hr', 'blockquote', 'pre', 'code'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && purgecss,
+    // ...
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
+    colors: {
+      // Ignoring yellow, green, teal, purple, pink
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      orange: colors.orange,
+      blue: colors.blue,
+      indigo: colors.indigo,
+    }
+  },
+  variants: {},
+  corePlugins: {
+    cursor: false,
+    float: false,
+    zIndex: false,
+  }
+}
